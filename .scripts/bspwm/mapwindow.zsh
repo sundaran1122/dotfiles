902b66c07466978485bfc984a1d44054b9dc7a31
sundaran1122 <sundaran11220@gmail.com>
2021-02-16 22:48:20 +0530

    small script changes

--- .scripts/bspwm/mapwindow.zsh
+++ .scripts/bspwm/mapwindow.zsh
@@ -1,18 +1,17 @@
 #!/bin/zsh
 
-windows=""
+windowname=""
 
 for window in $(bspc query -N -n .hidden.window -d focused)
 do
-  windows="$windows\n$(xdotool getwindowname $window)"
+  windowname="$windowname\n$(xdotool getwindowname $window)"
 done
 
-windowtomap=$(echo -e $windows | dmenu -nb "#2E3440" -nf "#D8DEE9" \
-  -sb "#3B4252" -sf "#ECEFF4" \
-  -fn "Mononoki" -p "map:" -i -l 20 \
-)
+windowid=$(echo -e $windowname | dmenu -nb "#2E3440" -nf "#D8DEE9" \
+  -sb "#3B4252" -sf "#ECEFF3" \
+  -fn "Mononokai" -p "map:" -i)
 
-for window in $(xdotool search --name $windowtomap)
+for window in $(xdotool search  --desktop $(xdotool get_desktop) --name $windowid)
 do
-bspc node $window -g hidden=false
+  bspc node $window -g hidden=false
 done
