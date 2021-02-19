97c070a8d6a1793c4dee78cf6869b127af2e8529
sundaran1122 <sundaran11220@gmail.com>
2021-02-20 02:14:30 +0530

    new features

---
+++ .scripts/dmenu/killproc.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+kill -$1 $(ps aux | awk '{print $2 " " $11}' | \
+	dmenu -nb "#2E3440" -nf "#D8DEE9" \
+  -sb "#3B4252" -sf "#ECEFF3" \
+  -fn "Mononokai" -p "run:" -i -l 10 \
+  | cut -d " " -f 1)
