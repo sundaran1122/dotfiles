f1821d00357181e3d0f64a1e2c2b5f0f6e3e4709
sundaran1122 <sundaran11220@gmail.com>
2021-02-28 18:01:58 +0530

    small changes

--- .scripts/general/notification-server.zsh
+++ .scripts/general/notification-server.zsh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-tiramisu | while read -r notif
+tiramisu -j | while read -r notif
 do
-  summary=$(echo $notif | grep ^summary | cut -d: -f 2)
-  [[ $summary == "" ]] && continue
-  yad --text=$summary --title=Dialog \
-    --geometry=-10+20+30x10 --no-focus \
+  summary=$(echo $notif | jq '.summary' | cut -d\" -f 2)
+  body=$(echo $notif | jq '.body' | cut -d\" -f 2)
+  yad --form --field=$body:LBL --text="$summary" \
+    --text-align=center --title=Dialog \
+    --geometry=-10+20+0x0 --no-focus \
     --no-buttons --timeout=1
   sleep 1
 done
